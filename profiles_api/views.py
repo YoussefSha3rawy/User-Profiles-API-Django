375d9bebd91a078196daf7925aef77902c8bdbad
Youssef Shaarawy <youssefshaarawy@gmail.com>
2023-06-06 23:34:43 +0300

    Added helloapiView Added Post

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from profiles_api import serializers
+
+
+class HelloApiView(APIView):
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None) -> Response:
+        result_list = [1, 2, 3, 4]
+        return Response({'message': 'Hello There', 'list': result_list})
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        name = serializer.validated_data.get('name')
+        message = f'Hello {name}!'
+        return Response({'message': message})
