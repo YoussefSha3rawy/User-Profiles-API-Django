58f10c64b04600a78d08c4078760fda7ac722676
Youssef Shaarawy <youssefshaarawy@gmail.com>
2023-06-06 22:34:02 +0300

    Enabled django admin for user profile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -9,14 +9,14 @@ class UserProfileManager(BaseUserManager):
         if not email:
             raise ValueError('User must have an email')
         email = self.normalize_email(email)
-        user = self.model(email, name)
+        user: UserProfile = self.model(email=email, name=name)
 
         user.set_password(password)
         user.save(using = self._db)
 
         return user
     def create_superuser(self, email, name, password):
-        user = self.create_user(email, name, password)
+        user: UserProfile = self.create_user(email=email, name=name, password=password)
 
         user.is_superuser = True
         user.is_staff = True
