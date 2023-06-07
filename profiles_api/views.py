d2462df8af0c0a3fbed905ffb156672eb710913e
Youssef Shaarawy <youssefshaarawy@gmail.com>
2023-06-07 15:55:26 +0300

    Added profiles api Added token authentication Added permissions

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,9 @@
 from rest_framework.response import Response
 from rest_framework import status, viewsets, views
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
 
-from profiles_api import serializers
+from profiles_api import serializers, models
 
 
 class HelloApiView(views.APIView):
@@ -75,3 +77,11 @@ class HelloViewSet(viewsets.ViewSet):
         """Handle removing an object"""
 
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating, creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
